31ec90ad3bc1024c82e01f743af894bc0b6aac7a
Habib1998 <Habib.kasbati@gmail.com>
2020-06-27 12:16:12 +0500

    Assignment 1

--- Assignment-2/src/Components/Heading2.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Heading2 = ({Heading2}) => {
-    return (
-        <div>
-            <h2>This is {Heading2}</h2>
-        </div>
-    )
-}
--- Assignment-3/src/Components/paragraph.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Paragraph = ({name}) => {
-    return (
-        <div>
-            This is a {name} component 
-        </div>
-    )
-}
